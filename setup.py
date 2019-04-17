b1b1701e532e320d06e0e59952a7e07a07797254
Kadamb Kaluskar <kadamb@kaluskar.com>
2019-04-17 12:42:34 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='ct_example',
     packages=['ct_example'],
     description='Hello world enterprise edition',
-    version='0.5',
+    version='0.6',
     url='http://github.com/example/linode_example',
     author='CT',
     author_email='kadamb.kaluskar@cleartax.in'
